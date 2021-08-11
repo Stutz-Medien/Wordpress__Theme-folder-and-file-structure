090e33ec642d09bcdc64ac0ef64e54294b6cd8d7
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2021-08-11 14:43:10 +0200

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,7 @@
-The Theme should be widgetized as fully as possible. Any area in the layout that works like a widget (tag cloud, blogroll, list of categories) or could accept widgets (sidebar) should allow widgets.
 <?php
 /**
  * File Template for displaying single posts
- * 
+ * The Theme should be widgetized as fully as possible. Any area in the layout that works like a widget (tag cloud, blogroll, list of categories) or could accept widgets (sidebar) should allow widgets.
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  */
 ?>
